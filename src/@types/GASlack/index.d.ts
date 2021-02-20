9570502dcd1a09a04afa5a17a8cf5fc72dedd92a
namahu <neti0326@gmail.com>
2021-02-20 09:14:26 +0900

    Adding a Type Definition.

--- src/@types/GASlack/index.d.ts
+++ src/@types/GASlack/index.d.ts
@@ -104,6 +104,51 @@ declare namespace GASlack {
   }
 
   namespace UsersService {
+    interface MemberProfile {
+      avatar_hash: string;
+      status_text: string;
+      status_emoji: string;
+      real_name: string;
+      display_name: string;
+      real_name_normalized: string;
+      display_name_normalized: string;
+      email: string;
+      image_24: string;
+      image_32: string;
+      image_48: string;
+      image_72: string;
+      image_192: string;
+      image_512: string;
+      team: string;
+    }
+    interface Member {
+      id: string;
+      team_id: string;
+      name: string;
+      deleted: boolean;
+      color: string;
+      real_name: string;
+      tz: string;
+      tz_label: string;
+      tz_offset: number;
+      profile: MemberProfile;
+      is_admin: boolean;
+      is_owner: boolean;
+      is_primary_owner: boolean;
+      is_restricted: boolean;
+      is_ultra_restricted: string;
+      is_bot: boolean;
+      updated: number;
+      is_app_user: boolean;
+      has_2fa: boolean;
+    }
+    interface UsersListResponse extends SlackAPIResponse {
+      members: Member[];
+      cache_ts: number;
+      response_metadata: {
+        next_cursor: string;
+      };
+    }
     interface UsersListParams {
       limit?: number;
       include_locale?: boolean;
